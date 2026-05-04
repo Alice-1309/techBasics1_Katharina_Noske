5ab8d358e930b651b167b4a666c7930c88b31c68
Alice-22.01 <katharina.noske@stud.leuphana.de>
2026-05-04 15:46:50 +0200

    assignments

---
+++ assignment_week_2_Katharina_Noske.py
@@ -0,0 +1,9 @@
+# Which plant is right for me?
+
+# ChatAI generated me the best conditions for the plants
+
+name = input("What is your name?")
+
+print("Hello", name, "Together we will find the best plant for you!")
+
+light = input("How much light does the plant become in the spot?")
